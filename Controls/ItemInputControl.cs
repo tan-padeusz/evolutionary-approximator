a2b163485bc72af0f847bfdc7d6bbc3b3cdbde6b
Maciej Moryń <moryn.maciej@protonmail.com>
2023-04-17 14:08:14 +0200

    added custom numeric input control

---
+++ Controls/ItemInputControl.cs
@@ -0,0 +1,37 @@
+﻿namespace Controls;
+
+public class ItemInputControl
+{
+    private Label LabelControl { get; } = new Label();
+    private ComboBox ValueControl { get; } = new ComboBox();
+
+    public void ConfigureControl(string name, int xPosition, int yPosition)
+    {
+        const int controlWidth = 300;
+        const int controlHeight = 30;
+        const float fontSize = 16;
+        this.LabelControl.Font = new Font(FontFamily.GenericMonospace, fontSize);
+        this.LabelControl.Location = new Point(xPosition, yPosition);
+        this.LabelControl.Size = new Size(controlWidth, controlHeight);
+        this.LabelControl.Text = name;
+        this.LabelControl.TextAlign = ContentAlignment.MiddleCenter;
+        this.ValueControl.DropDownStyle = ComboBoxStyle.DropDownList;
+        this.ValueControl.Font = new Font(FontFamily.GenericMonospace, fontSize);
+        this.ValueControl.Location = new Point(xPosition, yPosition + controlHeight);
+        this.ValueControl.Size = new Size(controlWidth, controlHeight);
+    }
+
+    public void Populate(Array values)
+    {
+        foreach (var value in values)
+        {
+            
+        }
+    }
+    
+    private class EnumItem
+    {
+        public string Name { get; }
+        public object Value { get; }
+    }
+}
---
+++ Controls/NumericInputControl.cs
@@ -0,0 +1,40 @@
+﻿namespace Controls;
+
+public class NumericInputControl
+{
+    private Label LabelControl { get; } = new Label();
+    private NumericUpDown ValueControl { get; } = new NumericUpDown();
+
+    public void ConfigureControl(string name, int xPosition, int yPosition)
+    {
+        const int controlWidth = 300;
+        const int controlHeight = 30;
+        const float fontSize = 16;
+        this.LabelControl.Font = new Font(FontFamily.GenericMonospace, fontSize);
+        this.LabelControl.Location = new Point(xPosition, yPosition);
+        this.LabelControl.Size = new Size(controlWidth, controlHeight);
+        this.LabelControl.Text = name;
+        this.LabelControl.TextAlign = ContentAlignment.MiddleCenter;
+        this.ValueControl.Font = new Font(FontFamily.GenericMonospace, fontSize);
+        this.ValueControl.Location = new Point(xPosition, yPosition + controlHeight);
+        this.ValueControl.Size = new Size(controlWidth, controlHeight);
+    }
+
+    public void ConfigureControlConstraints(int minimalValue, int maximalValue, int defaultValue, int deltaValue)
+    {
+        this.ValueControl.Increment = deltaValue;
+        this.ValueControl.Maximum = maximalValue;
+        this.ValueControl.Minimum = minimalValue;
+        this.ValueControl.Value = defaultValue;
+    }
+
+    public Control[] GetControls()
+    {
+        return new Control[] { this.LabelControl, this.ValueControl };
+    }
+
+    public int GetValue()
+    {
+        return (int) this.ValueControl.Value;
+    }
+}
